3833a812e30889c8b968dcc20c4a85a98e3fca0f
Pablo Rodrigues <pablo.rodriguesb@gmail.com>
2025-04-24 14:45:18 -0300

    feat: implementa entidade Usuario com relacionamentos

--- src/main/java/com/devpablo/taskmanager/model/Usuario.java
+++ src/main/java/com/devpablo/taskmanager/model/Usuario.java
@@ -1,4 +1,29 @@
 package com.devpablo.taskmanager.model;
 
-public class User {
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @OneToMany(mappedBy = "responsavel", cascade = CascadeType.ALL)
+    private List<Tarefa> tarefas = new ArrayList<>();
+
+
 }
