b5aa01f35fc51bbfa09579baf11c0b2a258dbbbc
Pablo Rodrigues <pablo.rodriguesb@gmail.com>
2025-04-24 14:45:36 -0300

    feat: implementa entidade Tarefa com status, prioridade e categoria

--- src/main/java/com/devpablo/taskmanager/model/Tarefa.java
+++ src/main/java/com/devpablo/taskmanager/model/Tarefa.java
@@ -1,4 +1,45 @@
 package com.devpablo.taskmanager.model;
 
-public class Task {
+import com.devpablo.taskmanager.enums.CategoriaTarefa;
+import com.devpablo.taskmanager.enums.PrioridadeTarefa;
+import com.devpablo.taskmanager.enums.StatusTarefa;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tarefas")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(length = 1000)
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    private StatusTarefa status;
+
+    @Enumerated(EnumType.STRING)
+    private PrioridadeTarefa prioridade;
+
+    @Enumerated(EnumType.STRING)
+    private CategoriaTarefa categoria;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_conclusao")
+    private LocalDateTime dataConclusao;
+
+    @Column(name = "data_vencimento")
+    private LocalDateTime dataVencimento;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario responsavel;
 }
